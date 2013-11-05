fe9adb8a6d29b512460ca6b5bb8da138293901d9
Stephen Schlie <heschlie@gmail.com>
2013-11-05 15:24:08 -0800

    replaced 'tiledtmxloader' with 'tmx' and got the player spawn and viewport working, need to check for correct world locations still

--- level.py
+++ level.py
@@ -1,8 +1,9 @@
 import pygame, sys, player
 from pygame.locals import *
 from pixelperfect import *
-import tiledtmxloader
-#from main import *
+import tmx
+import os
+
 
 def load(world):
     dt = 60
@@ -12,33 +13,17 @@ def load(world):
     
     print world
     clock = pygame.time.Clock()
-    hero = player.Player()
-    lvl = Level(hero)
-    
-    lvl.set_player_loc(hero, 100, 100)
-    lvl.sprite_layers[1].add_sprite(hero._sprite)
+    lvl = Level(screen)
     
     while True:
         clock.tick(dt)
-       
-        update_viewport(hero, lvl)
-        hero.update(dt/1000., lvl)
         
-        print lvl.renderer.pick_layer(lvl.sprite_layers[0], hero._sprite.rect.centerx, hero._sprite.rect.centery)
-        #print hero._sprite.rect.centerx, hero._sprite.rect.centery
-        #print hero.rect.centerx, hero.rect.centery
-        print hero._sprite.rect.centerx, hero._sprite.rect.centery
-        
+        lvl.tilemap.update(dt/1000., lvl)
         screen.fill((0,100,0))
-        
-        for layer in lvl.sprite_layers:
-            if layer.is_object_group:
-                continue
-            else:
-                lvl.renderer.render_layer(screen, layer)
+        lvl.tilemap.draw(screen)
         
         #screen.blit(lvl.lvl_bg, lvl.rect)
-        screen.blit(hero.image, hero.rect)
+        #screen.blit(hero.image, hero.rect)
         pygame.display.update()
         
         for event in pygame.event.get():
@@ -46,56 +31,27 @@ def load(world):
                 pygame.quit()
                 sys.exit()
                 
-def update_viewport(hero, lvl):
-    if hero.rect.x <= 640 and hero.rect.y <= 370:
-        #Top left
-        lvl.renderer.set_camera_position(640, 370)
-    elif hero.rect.x >= lvl.lvl_map.pixel_width - 640 and hero.rect.y >= lvl.lvl_map.pixel_height - 370:
-        #Bottom right
-        lvl.renderer.set_camera_position(lvl.lvl_map.pixel_width - 640, lvl.lvl_map.pixel_height - 370)
-    elif hero.rect.x <= 640 and hero.rect.y >= lvl.lvl_map.pixel_height - 370:
-        #Bottom left
-        lvl.renderer.set_camera_position(640, lvl.lvl_map.pixel_height - 370)
-    elif hero.rect.x >= lvl.lvl_map.pixel_width - 640 and hero.rect.y <= 370:
-        #Top Right
-        lvl.renderer.set_camera_position(lvl.lvl_map.pixel_width - 640, 370)
-    elif hero.rect.x <= 640:
-        #Left
-        lvl.renderer.set_camera_position(640, hero.rect.y)
-    elif hero.rect.x >= lvl.lvl_map.pixel_width - 640:
-        #Right
-        lvl.renderer.set_camera_position(lvl.lvl_map.pixel_width - 640, hero.rect.y)
-    elif hero.rect.y <= 370:
-        #Top
-        lvl.renderer.set_camera_position(hero.rect.x, 370)
-    elif hero.rect.y >= lvl.lvl_map.pixel_height - 370:
-        #bottom
-        lvl.renderer.set_camera_position(hero.rect.x, lvl.lvl_map.pixel_height -370)
-    else:
-        lvl.renderer.set_camera_position(hero.rect.x, hero.rect.y)
-
 class Level(object):
     
-    def __init__(self, hero):
+    def __init__(self, screen):
+        
+        dt = 60
 
         #self.lvl_bg, self.rect = load_image('test_level.png', None, True)
         #self.hitmask = pygame.surfarray.array_alpha(self.lvl_bg)
+        
         self.level = "levels/test/untitled.tmx"
-        self.lvl_map = tiledtmxloader.tmxreader.TileMapParser().parse_decode(self.level)
         
-        self.resources = tiledtmxloader.helperspygame.ResourceLoaderPygame()
-        self.resources.load(self.lvl_map)
+        self.tilemap = tmx.load(self.level, screen.get_size())
+        self.sprites = tmx.SpriteLayer()
+        self.start_cell = self.tilemap.layers['triggers'].find('player')[0]
         
-        self.renderer = tiledtmxloader.helperspygame.RendererPygame()
+        self.hero = player.Player(self, (self.start_cell.px, self.start_cell.py), self.sprites)
         
-        cam_x = hero._sprite.rect.centerx
-        cam_y = hero._sprite.rect.centery
         
-        self.renderer.set_camera_position_and_size(cam_x, cam_y, 1280, 720)
+        self.tilemap.layers.append(self.sprites)
         
-        self.sprite_layers = tiledtmxloader.helperspygame.get_layers_from_map(self.resources)
-        self.sprite_layers = [layer for layer in self.sprite_layers if not layer.is_object_group]
-                
+    def set_player_loc(self, player, loc):
+        player.rect.center = loc
         
-    def set_player_loc(self, player, x, y):
-        player.rect.center = (x, y)
+        
--- player.py
+++ player.py
@@ -1,11 +1,11 @@
 import pygame, sys
 from pygame.locals import *
 from pixelperfect import *
-import tiledtmxloader
+import tmx
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        #super(Player, self).__init__(*groups)
+    def __init__(self, lvl, loc, *groups):
+        super(Player, self).__init__(*groups)
         
         self.image, self.rect = load_image('images/player.png', None, True)
         self.hitmask = pygame.surfarray.array_alpha(self.image)
@@ -15,36 +15,36 @@ class Player(pygame.sprite.Sprite):
         self.flrA = Detectors(self.rect.x+1, self.rect.y, 1,self.rect.height+16)
         self.flrB = Detectors(self.rect.right-2, self.rect.y, 1,self.rect.height+16)
         self.wall = Detectors(self.rect.x, self.rect.bottom-30, self.rect.width, 1)
-        self._sprite = tiledtmxloader.helperspygame.SpriteLayer.Sprite(self.image, self.rect)
-        self.herox = 100
-        self.heroy = 100
+        #self._sprite = tiledtmxloader.helperspygame.SpriteLayer.Sprite(self.image, self.rect)
+        #self.herox, self.heroy = loc
+        lvl.set_player_loc(self, (loc))
         
     def update(self, dt, lvl):
         last = self.rect.copy()
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            #self._sprite.rect.centerx -= self.speed * dt
-            self.herox -= self.speed * dt
+            self.rect.centerx -= self.speed * dt
+            #self.herox -= self.speed * dt
         if key[pygame.K_RIGHT]:
-            #self._sprite.rect.centerx += self.speed * dt
-            self.herox += self.speed * dt
+            self.rect.centerx += self.speed * dt
+            #self.herox += self.speed * dt
         if key[pygame.K_DOWN]:
-            #self._sprite.rect.centery += self.speed * dt
-            self.heroy += self.speed * dt
+            self.rect.centery += self.speed * dt
+            #self.heroy += self.speed * dt
         if key[pygame.K_UP]:
-            #self._sprite.rect.centery -= self.speed * dt
-            self.heroy -= self.speed * dt
+            self.rect.centery -= self.speed * dt
+            #self.heroy -= self.speed * dt
         if self.resting and key[pygame.K_SPACE]:
             self.dy = -200
         #self.dy = min(400, self.dy + 20)
         self.rect.y += self.dy * dt
-        self.rect.centerx = self.herox
-        self.rect.centery = self.heroy
+        
+        lvl.tilemap.set_focus(self.rect.centerx, self.rect.centery)
         
         
-        if check_collision(self.flrA, lvl):
-            print "Collision detected"
+        #if check_collision(self.flrA, lvl):
+        #    print "Collision detected"
             
 class Detectors(object):
     def __init__(self, x, y, width, height):

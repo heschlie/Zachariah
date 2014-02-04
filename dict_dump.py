0dddee0ed39654da03f9cbd136f07011ea505cb0
Stephen Schlie <heschlie@gmail.com>
2014-02-04 12:08:05 -0800

    Adjusted the direction vars for the player.  Changed player.direction to player.direction_x and added player.direction_y.  This allows them to both be set independently and allows for the player to drop through platforms while moving on the x axis and not jump

---
+++ dict_dump.py
@@ -0,0 +1,12 @@
+import monster
+import npc
+
+monsters = {
+    'standing': monster.Standing,
+    'walker': monster.Walker,
+    'jumper': monster.Jumper
+    }
+
+npc = {
+        'dino_male': npc.DinoMale
+    }

5605649f0d4e9f90fc34c1b6188084923217d8c1
Stephen Schlie <heschlie@gmail.com>
2014-02-15 21:36:42 -0800

    trimmed out obsolete code

--- dict_dump.py
+++ dict_dump.py
@@ -16,6 +16,6 @@ levels = {
         'name': 'test',
         'para': {0: 'images/backgrounds/clouds.png', 1: 'images/backgrounds/mid-BG - forest.png', 2: 'images/backgrounds/mid-BG - forest2.png'},
         'para_speed': {0: 6, 1: 4, 2: 2},
-        'para_start': {0: (0, -169), 1: (0, 120), 2: (0, 120) }
+        'para_start': {0: (0, -70), 1: (0, 120), 2: (0, 120) }
     }
 }
